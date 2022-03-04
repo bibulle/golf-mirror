c0c82562cb85fcd037800a8edf8e6c8f2e7f74e2
m341772 <eric.martin@airfrance.fr>
2022-03-04 10:42:10 +0100

    fix: reog import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ViewerEffects } from './store/viewer/viewer.effects';
-import * as ViewerReducer from "./store/viewer/viewer.reducer"
-import { BrowserParamsService } from './utils/browser-params.service';
-import { HomeComponent } from './home/home.component';
-import { ViewerComponent } from './viewer/viewer.component';
 import { PoseModule } from './pose/pose.module';
-import { VideoModule } from './video/video.module';
 import { MyStoreModule } from './store/my-store.module';
+import { BrowserParamsService } from './utils/browser-params.service';
+import { VideoModule } from './video/video.module';
+
 
 @NgModule({
   declarations: [
